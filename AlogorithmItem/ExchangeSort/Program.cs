29a8977f6c964ed8c3ae1c3a2a52af849b17e9c0
LuckyMechanic <lqy1024@foxmail.com>
2023-11-13 19:52:37 +0800

    交换排序之冒泡排序

---
+++ AlogorithmItem/ExchangeSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = {3,6,5,7,9,1,8};
+        Console.WriteLine("原始数组：");
+        foreach (int item in array)
+        {
+            Console.Write(item+" ");
+        }
+        Console.WriteLine();
+
+        BubbleSort(array);
+        Console.WriteLine("冒泡排序后的数组:");
+        foreach(int i in array)
+        {
+            Console.Write(i+" ");
+        }
+    }
+
+    static void BubbleSort(int[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n-i-1; j++)
+            {
+                if(array[j]>array[j+1])
+                {
+                     int temp = array[j];
+                     array[j] = array[j + 1];
+                     array[j+1] = temp;
+                }    
+            }
+        }
+    }
+}

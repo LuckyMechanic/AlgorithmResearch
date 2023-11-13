6cb1168b29ef4d4a7520a15e8526246f1981f189
LuckyMechanic <lqy1024@foxmail.com>
2023-11-13 20:55:42 +0800

    交换排序 之 快速排序

--- AlogorithmItem/ExchangeSort/Program.cs
+++ AlogorithmItem/ExchangeSort/Program.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Data;
 
+/// <summary>
+/// 交换排序
+/// </summary>
 class Program
 {
+    
+    //冒泡排序
+    /*
     static void Main(string[] args)
     {
         int[] array = {3,6,5,7,9,1,8};
@@ -36,4 +43,60 @@ class Program
             }
         }
     }
+   
+ */
+    //快速排序
+    static void Main(string[] args)
+    {
+        int[] array = {10, 7, 8, 9, 1, 5};
+        Console.WriteLine("原始数组：");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+        QuickSort(array,0,array.Length - 1);
+        Console.WriteLine("快速排序后数组：");
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    static void QuickSort(int[] arr,int low,int hight)
+    {
+        if(low < hight)
+        {
+            int pivot = Partition(arr,low,hight);
+            QuickSort(arr,low,pivot-1);
+            QuickSort(arr,pivot +1,hight);
+        }
+    }
+
+    static int Partition(int[] arr,int low,int hight)
+    {
+        int pivot = arr[hight];
+        int i = (low - 1);
+        for (int j = low; j < hight; j++)
+        {
+            if(arr[j]<=pivot)
+            {
+                i++;
+                Swap(arr,i,j);
+            }
+        }
+        Swap(arr,i+1,hight);
+        return i + 1;
+    }
+
+    static void Swap(int[] arr,int a,int b)
+    {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+   
+
+
 }
